f91f90f059c1db182071ed02e8a6a54abe0b50bc
shamp0erna99 <58555236+eternalsec@users.noreply.github.com>
2020-05-10 20:01:04 +0700

    Add files via upload

---
+++ Nged0rK.py
@@ -0,0 +1,215 @@
+#!usr/bin/env python3
+#author by shamp0erna99
+
+import requests
+import re
+import optparse
+import subprocess
+import time
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-u", "--url", dest="url", help="URL")
+    parser.add_option('-m', '--manual', help="Enables manual mode", action="store_true")
+    (option, arguments) = parser.parse_args()
+    if not option.url:
+        parser.error("[-] Please specify an URL, use --help for more info")
+    return option
+options =  get_arguments()
+url = options.url
+print("\n\n")
+subprocess.call(["chmod", "777", url + ".html"])
+subprocess.call(["rm", url + ".html"])
+subprocess.call(["clear"])
+print("           /$$   /$$                           /$$  /$$$$$$            /$$   /$$    ")
+print("          | $$$ | $$                          | $$ /$$$_  $$          | $$  /$$/   ")
+print("          | $$$$| $$  /$$$$$$   /$$$$$$   /$$$$$$$| $$$$\ $$  /$$$$$$ | $$ /$$/    ")
+print("          | $$ $$ $$ /$$__  $$ /$$__  $$ /$$__  $$| $$ $$ $$ /$$__  $$| $$$$$/     ")
+print("          | $$  $$$$| $$  \ $$| $$$$$$$$| $$  | $$| $$\ $$$$| $$  \__/| $$  $$     ")
+print("          | $$\  $$$| $$  | $$| $$_____/| $$  | $$| $$ \ $$$| $$      | $$\  $$    ")
+print("          | $$ \  $$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$/| $$      | $$ \  $$   ")
+print("          |__/  \__/ \____  $$ \_______/ \_______/ \______/ |__/      |__/  \__/   ")
+print("                           |$$  -------------------->eternalsec                    ")                          
+print("                    |$$$$$$$$/                                                     ")
+print("                    \_______/                                                      ")
+print("\n\n")
+f = open(str(url) + ".html", "at")
+f.write(
+    '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"> <title>LazyDorking a.k.a eternalsec</title> </head> <body><br>')
+
+if options.manual:
+    def manual():
+        global f
+        print("[+]Registering data into the file.\n")
+        time.sleep(2)
+        f.write("<h2>Manual mode - Check the links manually.</h2>")
+        f.write("<br>")
+        f.write('<h2>Result Directory listing</h2>')
+        f.write('<a href="https://www.google.com/search?q=site:'+ url +'+intitle:index.of&hl=en">Click Here</a>')
+        f.write("<br>")
+        f.write('<h2>Result Configuration files</h2>')
+        f.write('<a href="https://www.google.com/search?q=site:'+ url +'+ext:xml+|+ext:conf+|+ext:cnf+|+ext:reg+|+ext:inf+|+ext:rdp+|+ext:cfg+|+ext:txt+|+ext:ora+|+ext:ini&hl=en">Click Here</a>')
+        f.write("<br>")
+        f.write('<h2>Result Database files</h2>')
+        f.write('<a href="https://www.google.com/search?q=site:'+ url +'+ext:sql+|+ext:dbf+|+ext:mdb&hl=en">Click Here</a>')
+        f.write("<br>")
+        f.write('<h2>Result Log files</h2>')
+        f.write('<a href="https://www.google.com/search?q=site:'+ url +'+ext:log&hl=en">Click Here</a>')
+        f.write("<br>")
+        f.write('<h2>Result Backup and Old files</h2>')
+        f.write('<a href="https://www.google.com/search?q=site:'+ url +'+ext:bkf+|+ext:bkp+|+ext:bak+|+ext:old+|+ext:backup&hl=en">Click Here</a>')
+        f.write("<br>")
+        f.write('<h2>Result Login pages</h2>')
+        f.write('<a href="https://www.google.com/search?q=site:'+ url +'+intext:login&hl=en">Click Here</a>')
+        f.write("<br>")
+        f.write('<h2>Result SQL Errors</h2>')
+        f.write('<a href="https://www.google.com/search?q=site:'+ url +'+intext:%22sql+syntax+near%22+|+intext:%22syntax+error+has+occurred%22+|+intext:%22incorrect+syntax+near%22+|+intext:%22unexpected+end+of+SQL+command%22+|+intext:%22Warning:+mysql_connect()%22+|+intext:%22Warning:+mysql_query()%22+|+intext:%22Warning:+pg_connect()%22&hl=en">Click Here</a>')
+        f.write("<br>")
+        f.write('<h2>Result Publicly Exposed Documents</h2>')
+        f.write('<a href="https://www.google.com/search?q=site:'+ url +'+ext:doc+|+ext:docx+|+ext:odt+|+ext:pdf+|+ext:rtf+|+ext:sxw+|+ext:psw+|+ext:ppt+|+ext:pptx+|+ext:pps+|+ext:csv&hl=en">Click Here</a>')
+        f.write("<br>")
+        f.write('<h2>phpinfo()</h2>')
+        f.write('<a href="https://www.google.com/search?q=site:'+ url +'+ext:php+intitle:phpinfo+%22published+by+the+PHP+Group%22&hl=en">Click Here</a>')
+        f.write("<br>")
+        f.close()
+        print("File successfully created.\n")
+    manual()
+
+else:
+
+    def process_google():
+        global f
+        print("Dorking via Google")
+        print("\n")
+        f.write("<h1>Results from Google</h1>")
+        f.write("<br>")
+        print ("[#]Checking for Directory listing vulnerabilities")
+        requesturl = 'https://www.google.com/search?q=site:'+ url +'+intitle:index.of&hl=en'
+        response = requests.get(requesturl)
+        notfound = re.search('\s-\sdid not match any documents.', response.text)
+        captcha = re.search(',\ssolving the above CAPTCHA will let you continue\s', response.text)
+        if notfound:
+            print("[-]No results found\n")
+        elif captcha:
+            print("[-]Captcha triggered. Please try after some time.\n")
+        else:
+            print("[+]Registering data into the file.\n")
+            f.write('<h2>Result Directory listing</h2>')
+            f.write('<a href="' + requesturl + '">Click Here</a>')
+            f.write("<br>")
+        print ("[#]Checking for Configuration files exposed")
+        requesturl = 'https://www.google.com/search?q=site:'+ url +'+ext:xml+|+ext:conf+|+ext:cnf+|+ext:reg+|+ext:inf+|+ext:rdp+|+ext:cfg+|+ext:txt+|+ext:ora+|+ext:ini&hl=en'
+        response = requests.get(requesturl)
+        notfound = re.search('\s-\sdid not match any documents.', response.text)
+        captcha = re.search(',\ssolving the above CAPTCHA will let you continue\s', response.text)
+        if notfound:
+            print("[-]No results found\n")
+        elif captcha:
+            print("[-]Captcha triggered. Please try after some time.\n")
+        else:
+            print("[+]Registering data into the file.\n")
+            f.write('<h2>Result Configuration files</h2>')
+            f.write('<a href="' + requesturl + '">Click Here</a>')
+            f.write("<br>")
+        print ("[#]Checking for Database files exposed")
+        requesturl = 'https://www.google.com/search?q=site:'+ url +'+ext:sql+|+ext:dbf+|+ext:mdb&hl=en'
+        response = requests.get(requesturl)
+        notfound = re.search('\s-\sdid not match any documents.', response.text)
+        captcha = re.search(',\ssolving the above CAPTCHA will let you continue\s', response.text)
+        if notfound:
+            print("[-]No results found\n")
+        elif captcha:
+            print("[-]Captcha triggered. Please try after some time.\n")
+        else:
+            print("[+]Registering data into the file.\n")
+            f.write('<h2>Result Database files</h2>')
+            f.write('<a href="' + requesturl + '">Click Here</a>')
+            f.write("<br>")
+        print ("[#]Checking for Log files exposed")
+        requesturl = 'https://www.google.com/search?q=site:'+ url +'+ext:log&hl=en'
+        response = requests.get(requesturl)
+        notfound = re.search('\s-\sdid not match any documents.', response.text)
+        captcha = re.search(',\ssolving the above CAPTCHA will let you continue\s', response.text)
+        if notfound:
+            print("[-]No results found\n")
+        elif captcha:
+            print("[-]Captcha triggered. Please try after some time.\n")
+        else:
+            print("[+]Registering data into the file.\n")
+            f.write('<h2>Result Log files</h2>')
+            f.write('<a href="' + requesturl + '">Click Here</a>')
+            f.write("<br>")
+        print ("[#]Checking for Backup and old files")
+        requesturl = 'https://www.google.com/search?q=site:'+ url +'+ext:bkf+|+ext:bkp+|+ext:bak+|+ext:old+|+ext:backup&hl=en'
+        response = requests.get(requesturl)
+        notfound = re.search('\s-\sdid not match any documents.', response.text)
+        captcha = re.search(',\ssolving the above CAPTCHA will let you continue\s', response.text)
+        if notfound:
+            print("[-]No results found\n")
+        elif captcha:
+            print("[-]Captcha triggered. Please try after some time.\n")
+        else:
+            print("[+]Registering data into the file.\n")
+            f.write('<h2>Result Backup and Old files</h2>')
+            f.write('<a href="' + requesturl + '">Click Here</a>')
+            f.write("<br>")
+        print ("[#]Checking for Login pages")
+        requesturl = 'https://www.google.com/search?q=site:'+ url +'+inurl:login&hl=en'
+        response = requests.get(requesturl)
+        notfound = re.search('\s-\sdid not match any documents.', response.text)
+        captcha = re.search(',\ssolving the above CAPTCHA will let you continue\s', response.text)
+        if notfound:
+            print("[-]No results found\n")
+        elif captcha:
+            print("[-]Captcha triggered. Please try after some time.\n")
+        else:
+            print("[+]Registering data into the file.\n")
+            f.write('<h2>Result Login pages</h2>')
+            f.write('<a href="' + requesturl + '">Click Here</a>')
+            f.write("<br>")
+        print ("[#]Checking for SQL errors")
+
+        requesturl = 'https://www.google.com/search?q=site:'+ url +'+intext:%22sql+syntax+near%22+|+intext:%22syntax+error+has+occurred%22+|+intext:%22incorrect+syntax+near%22+|+intext:%22unexpected+end+of+SQL+command%22+|+intext:%22Warning:+mysql_connect()%22+|+intext:%22Warning:+mysql_query()%22+|+intext:%22Warning:+pg_connect()%22&hl=en'
+        response = requests.get(requesturl)
+        notfound = re.search('\s-\sdid not match any documents.', response.text)
+        captcha = re.search(',\ssolving the above CAPTCHA will let you continue\s', response.text)
+        if notfound:
+            print("[-]No results found\n")
+        elif captcha:
+            print("[-]Captcha triggered. Please try after some time.\n")
+        else:
+            print("[+]Registering data into the file.\n")
+            f.write('<h2>Result SQL Errors</h2>')
+            f.write('<a href="' + requesturl + '">Click Here</a>')
+            f.write("<br>")
+        print("[#]Checking for Publicly exposed documents ")
+        requesturl = 'https://www.google.com/search?q=site:'+ url +'+ext:doc+|+ext:docx+|+ext:odt+|+ext:pdf+|+ext:rtf+|+ext:sxw+|+ext:psw+|+ext:ppt+|+ext:pptx+|+ext:pps+|+ext:csv&hl=en'
+        response = requests.get(requesturl)
+        notfound = re.search('\s-\sdid not match any documents.', response.text)
+        captcha = re.search(',\ssolving the above CAPTCHA will let you continue\s', response.text)
+        if notfound:
+            print("[-]No results found\n")
+        elif captcha:
+            print("[-]Captcha triggered. Please try after some time.\n")
+        else:
+            print("[+]Registering data into the file.\n")
+            f.write('<h2>Result Publicly Exposed Documents</h2>')
+            f.write('<a href="' + requesturl + '">Click Here</a>')
+            f.write("<br>")
+        print("[#]Checking for phpinfo() ")
+        requesturl = 'https://www.google.com/search?q=site:'+ url +'+ext:php+intitle:phpinfo+%22published+by+the+PHP+Group%22&hl=en'
+        response = requests.get(requesturl)
+        notfound = re.search('\s-\sdid not match any documents.', response.text)
+        captcha = re.search(',\ssolving the above CAPTCHA will let you continue\s', response.text)
+        if notfound:
+            print("[-]No results found\n")
+        elif captcha:
+            print("[-]Captcha triggered. Please try after some time.\n")
+        else:
+            print("[+]Registering data into the file.\n")
+            f.write('<h2>phpinfo()</h2>')
+            f.write('<a href="' + requesturl + '">Click Here</a>')
+            f.write("<br>")
+            f.close()
+
+    process_google()
